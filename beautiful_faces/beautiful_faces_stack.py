46aa41ea9a6bb51491539bfac26bf94a77f0672d
schirmerm <matthew.schirmer@ticoon.com>
2022-12-15 21:26:06 -0500

    Add dynamo table for face metadata

--- beautiful_faces/beautiful_faces_stack.py
+++ beautiful_faces/beautiful_faces_stack.py
@@ -16,17 +16,20 @@ class BeautifulFacesStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        bucket_name = _cfnParameter(self, "uploadBucketName", type="String",
-                                    description="The name of the Amazon S3 bucket where uploaded images will be stored.")
+        
         user_pool = _cognito.UserPool(self, "UserPool")
         user_pool.add_client("app-client",
             auth_flows=_cognito.AuthFlow(user_password=True),
             supported_identity_providers=[_cognito.UserPoolClientIdentityProvider.COGNITO]
         )
-        auth = _apigateway.CognitoUserPoolsAuthorizer(self, "imagesAuthorizer", cognito_user_pools=[user_pool])
-        my_table = _dynamodb.Table(self, id='dynamoTable', table_name='formmetadata', partition_key=_dynamodb.Attribute(
-            name='userid', type=_dynamodb.AttributeType.STRING)) #change primary key here
-        my_bucket = _s3.Bucket(self, id='s3bucket', bucket_name=bucket_name.value_as_string)
+        
+        auth = _apigateway.CognitoUserPoolsAuthorizer(self, "faceDetector", cognito_user_pools=[user_pool])
+
+        my_table = _dynamodb.Table(self, id='dynamoTable', table_name='beautiful_faces_metadata', partition_key=_dynamodb.Attribute(
+            name='faceid', type=_dynamodb.AttributeType.STRING))
+        
+        my_bucket = _s3.Bucket(self, id='s3bucket', bucket_name=f"beautifulfaces-upload-{self.account}")
+        
         my_lambda = _lambda.Function(self, id='lambdafunction', function_name="formlambda", runtime=_lambda.Runtime.PYTHON_3_7,
                                      handler='index.handler',
                                      code=_lambda.Code.from_asset(os.path.join("./", "lambda-handler")),
@@ -35,6 +38,7 @@ class BeautifulFacesStack(Stack):
                                          'table': my_table.table_name
                                         },
                                      )
+        
         my_bucket.grant_read_write(my_lambda)
         my_table.grant_read_write_data(my_lambda)
         my_api = _apigateway.LambdaRestApi(self, id='lambdaapi', rest_api_name='formapi', handler=my_lambda, proxy=True)
